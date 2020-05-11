fee283d401e4d5ae54219fbdddfc419a1345c6ba
Mayank Malik <mayankmalik012@gmail.com>
2020-05-11 18:53:06 +0530

    fixed practical23.pl

--- practicals/practical23.pl
+++ practicals/practical23.pl
@@ -4,4 +4,5 @@ is second ordered list and L3 represents the merged list.*/
 merge(L1,L2,L3):-
     sort(L1,OL1),
     sort(L2,OL2),
-    union(OL1,OL2,L3).
+    union(OL1,OL2,OL3),
+    sort(OL3,L3).
